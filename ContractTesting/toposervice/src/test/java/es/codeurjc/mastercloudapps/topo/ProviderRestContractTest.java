8903f9268ba8add3358e3d4620d98c1c78cb3b7e
juanmi <jmcabrera@copado.com>
2022-02-01 10:49:47 +0100

    Add buildPact.sh

--- ContractTesting/toposervice/src/test/java/es/codeurjc/mastercloudapps/topo/ProviderRestContractTest.java
+++ ContractTesting/toposervice/src/test/java/es/codeurjc/mastercloudapps/topo/ProviderRestContractTest.java
@@ -7,10 +7,10 @@ import au.com.dius.pact.provider.junit.Consumer;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.StateChangeAction;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
-import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import es.codeurjc.mastercloudapps.topo.controller.TopoController;
 import es.codeurjc.mastercloudapps.topo.model.City;
 import es.codeurjc.mastercloudapps.topo.service.TopoService;
@@ -18,23 +18,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Provider("topo-provider")
 @Consumer("planner-consumer-rest")
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 @PactFolder("./Pacts")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProviderRestContractTest {
 
   @InjectMocks
   private TopoController controller;
 
-  @Mock
+  @LocalServerPort
+  int port;
+
+  @MockBean
   private TopoService service;
 
   @TestTemplate
@@ -46,11 +54,14 @@ class ProviderRestContractTest {
   @BeforeEach
   void setUp(PactVerificationContext context) {
 
-//    Mockito.reset(service);
+    Mockito.reset(service);
 
-    MockMvcTestTarget testTarget = new MockMvcTestTarget();
-    testTarget.setControllers(controller);
-    context.setTarget(testTarget);
+    context.setTarget(new HttpTestTarget("localhost", port));
+
+//    MockMvcTestTarget testTarget = new MockMvcTestTarget();
+//    testTarget.setControllers(controller);
+//    context.setTarget(testTarget);
+
   }
 
   @State(value = "City 'Madrid' exists", action = StateChangeAction.SETUP)
